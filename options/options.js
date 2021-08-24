abc5ffea76761c1f58ce13d6a53cd678df63db23
Crowdbotics <team@crowdbotics.com>
2021-08-24 18:53:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cool_water_30081Identifier",
+  url: "https://cool_water_30081Identifier.botics.co",
+  api: "https://cool_water_30081Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
